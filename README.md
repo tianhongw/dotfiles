099fd972f6b1aabd6fa75364230710bc3a9e799e
KotiyaSanae <dream@trytwice.me>
2018-05-30 20:27:17 +0800

    update README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hosts the dotfiles i use in my daily life.
+### Dotfiles
+---
+This repository hosts the dotfiles i use in my daily life. They should be cloned to your home directory so that the path is ~/dotfiles/.The install.sh script does two things:  
+1. backup your config files to ~/dotfiles_old.(if exist)
+2. copy my config files for awesomewm & tmux & vim & urxvt & zsh to you.
+
+### Installation
+---
+git clone https://github.com/Trytwice/dotfiles.git ~/dotfiles && cd ~/dotfiles && ./install.sh
--- install.sh
+++ install.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
+######################
+# This script does two things:
+#   1.backup your config file to ~/dotfiles_old.(if exist)
+#   2.copy my config file for awesomewm & tmux & vim & urxvt & zsh to you.
+######################
 
-dir=~/dotfiles
-olddir=~/dotfiles_old
-files="config/awesome tmux.conf vimrc Xresources ycm_extra_conf.py zshrc"
+dir=~/dotfiles              #dotfiles directory
+olddir=~/dotfiles_old       #old dotfiles backup directory
+files="config/awesome tmux.conf vimrc Xresources zshrc"   #list of files
 
+# check if oh-my-zsh is installed
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "You need install oh-my-zsh,see https://github.com/robbyrussell/oh-my-zsh"
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
+# two awesome plugins for oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
+# check if Vundle is installed
 if [ ! -d ~/.vim/bundle ]; then
     echo "You need install Vundle, see git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+# change to the dotfiles directory
 cd $dir
 
+# backup old config files and copy my config files to you
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 for file in $files; do
@@ -26,6 +36,7 @@ for file in $files; do
     fi
     cp -r $file ~/.$file
 done
-vim +PluginInstall +qall
-echo "All done, restart awesomewm(Win + Ctrl + r) and open new terminal to see what happend"
+vim +PluginInstall +qall    #run this to install plugins for vim according to vimrc
+xrdb ~/.Xresources          #run this to make the .Xresources work without restart X.
+echo -e "...........................\nAll done, restart awesomewm(Win + Ctrl + r) and open new terminal to see what happend"
 
